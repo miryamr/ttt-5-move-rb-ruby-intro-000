73add4aa818677dde686b1bfe646976c1d078ed8
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-03 05:59:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,7 @@ def input_to_index(user_input)
    user_input.to_i - 1
 end
 
-def move(board_array, index, character = "X")
-  board[index] = player
+def update_array_at_with(board_array, index, character = "X")
 end
 
 
