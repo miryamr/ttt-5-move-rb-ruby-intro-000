2d50df06a2d8bb3924ae714a060f51567f52811e
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-03 06:05:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,5 +14,11 @@ end
 def move(board_array, index, character = "X")
 end
 
+it 'allows "X" player in the top left position' do
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  move(board, 0, "X")
+
+  expect(board).to eq(["X", " ", " ", " ", " ", " ", " ", " ", " "])
+end
 # The element at index 0 of array 'board' is set to the value "X"
 #=> ["X", " ", " "]
